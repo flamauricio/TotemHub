102f104326f622257f42f0f67b0f12f9ca42ed1d
VH13 <vitor.herculano@bandtec.com.br>
2021-11-20 20:35:57 -0300

    Novas alterações no botTotemHub (java assistente)

--- Java/java_install_assistant.sh
+++ Java/java_install_assistant.sh
@@ -5,7 +5,7 @@ NC='\033[0m'
 VERSAO=11
 	
 	echo  "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) Buenas, Buenas!!! Serei seu assistente para instalação do Java!;"
-	echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) Qual seu nome?"
+	echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) Como você gostaria de ser chamado?"
 	read userName
 	echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) Olá,${userName}! Podemos iniciar? (S/N)" 
 	read inicio
@@ -18,7 +18,7 @@ VERSAO=11
 	read mensage
 	if [ \"$mensage\" == \"s\" ]
 	then
-		echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) Entendi, irei atualizar agora para você!"
+		echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) Entendido, irei atualizar agora para você!"
 		sudo apt upgrade && sudo apt update
 	fi		
 		echo ""
@@ -33,36 +33,44 @@ VERSAO=11
 		sleep 1	
 		echo -ne $(tput setaf 10)'#######################   (100%)\r'
 		echo -ne '\n'
-
+       
 java -version
 if [ $? -eq 0 ]
 	then
-		echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) : Olá você já tem o java instalado!!"
+		echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) : Que bacana, você já possui o java instalado!"
 	else
-		echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7)  Opa! Não identifiquei nenhuma versão do Java instalado, mas sem problemas, irei resolver isso agora!"
-		echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7)  Confirme para mim se realmente deseja instalar o Java (S/N)?"
+		echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7)  Ops! Não encontrei nenhuma versão do Java instalado... "
+		echo ""
+		echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) Fique tranquilo, posso resolver isso para você ${userName}!"
+		echo ""
+		echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) ${userName}, confirme pra mim se você realmente deseja instalar o JAVA em sua máquina? (S/N)"
 	read inst
 	if [ \"$inst\" == \"s\" ]
 		then
-			echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7)  Ok! Você escolheu instalar o Java ;D"
-			echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7)  Adicionando o repositório!"
+			echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7)  Ok! Você optou por instalar o Java ;D"
+			echo ""
+			echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) 1° passo - Adicionando o repositório!"
 			sleep 2
 			sudo add-apt-repository ppa:webupd8team/java -y
 			clear
-			echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7)  Atualizando! Quase lá."
+			echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7)  Está Atualizando... Quase lá."
 			sleep 2
 			sudo apt update -y
 			clear
 			
 			if [ $VERSAO -eq 11 ]
 				then
-					echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) Preparando para instalar a versão 11 do Java. Confirme a instalação quando solicitado ;D"
+					echo " 2° Preparando a instalação da versão 11 do Java. "
+					echo ""
+					echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) ${userName} Quando for solicitado, Confirme a instalação por favor ;D"
 					sudo apt install default-jre ; apt install openjdk-11-jre-headless; -y
 					clear
-					echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) Java instalado com sucesso!"
+					echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7) O Java instalado com sucesso!"
+					echo ""
+					echo "Muito obrigado pela sua companhia, espero ter sido útil! valeu ${userName}"
 				fi
 		else 	
-		echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7)  Você optou por não instalar o Java por enquanto, até a próxima então!"
+		echo "$(tput setaf 10)[botTotemHub]:$(tput setaf 7)  Você optou por não instalar o Java por enquanto, até a próxima ${userName}!"
 	fi
 fi
 

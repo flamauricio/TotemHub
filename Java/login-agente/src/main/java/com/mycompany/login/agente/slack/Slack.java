b08a72db94a4ecc94dc9b5eef2cd38072f4a22b6
flamauricio <fla.mauricio.valerio17@gmail.com>
2021-12-08 23:39:47 -0300

    subindo login .jar

--- Java/login-agente/src/main/java/com/mycompany/login/agente/slack/Slack.java
+++ Java/login-agente/src/main/java/com/mycompany/login/agente/slack/Slack.java
@@ -14,7 +14,7 @@ import org.json.JSONObject;
  */
 public class Slack {
     private static final HttpClient client = HttpClient.newHttpClient();
-    private static final String URL = "https://hooks.slack.com/services/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";
+    private static final String URL = "https://hooks.slack.com/services/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";
 
     public static void sendMessage(JSONObject content) throws IOException, InterruptedException {
 
